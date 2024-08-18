a30d9778f03e3d1797389e639e741607f15453fd
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2024-08-18 19:38:12 +0300

    Initial Commit

---
+++ myHelper/myHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHelperApp.swift
+//  myHelper
+//
+//  Created by Андрей Банин on 18.8.24..
+//
+
+import SwiftUI
+
+@main
+struct myHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
